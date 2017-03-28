5963c7cbfe740f0464cde1e4e1706b2f0e69fd85
jereable <jere.kelonen@hotmail.com>
2017-03-28 15:42:36 +0300

    Testing websocket with simple input.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+var expressWs = require('express-ws')(app);
 
-app.set('port', (process.env.PORT || 80));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -13,3 +14,11 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+app.ws('/echo', function(ws, req) {
+  ws.on('message', function(message) {
+    ws.send(message);
+	console.log(message);
+  });
+});
+
+var server = app.listen(process.env.PORT || 5000);
