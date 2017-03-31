2a6fa0acc3db8ae84bad340d46d9366f5cdcac60
jereable <jere.kelonen@hotmail.com>
2017-03-31 15:19:19 +0300

    Working version of firstgame.

--- AirSketch Project/firstgame.js
+++ AirSketch Project/firstgame.js
@@ -4,32 +4,32 @@ switch(user) {
     case 'RUN':
         var run = prompt("Are you a fast runner? (YES or NO)").toUpperCase();
         if(run === 'YES') {
-            console.log("You start running and outrun the guard! I guess he should limit those donuts...");
+            confirm("You start running and outrun the guard! I guess he should limit those donuts...");
         }
             else {
-            console.log("You start running but the guard catches you! You smash your head on the ground, and the guard cuffs you! Poor guy...");
+            confirm("You start running but the guard catches you! You smash your head on the ground, and the guard cuffs you! Poor guy...");
             }
         break;
     case 'FIGHT':
         var fight = prompt("Are you a good fighter? (YES or NO)").toUpperCase();
         var strong = prompt("Are you strong? (YES or NO").toUpperCase();
         if(fight === 'YES' || strong === 'YES') {
-            console.log("You beat the shit out of the guard, and walk away like a boss! I guess you should hurry before backup arrives...");
+            confirm("You beat the shit out of the guard, and walk away like a boss! I guess you should hurry before backup arrives...");
         }
         else {
-        console.log("The guard makes a ninja move on you, and cuffs you! Some muay thai, or gym would make good for you...");
+        confirm("The guard makes a ninja move on you, and cuffs you! Some muay thai, or gym would make good for you...");
         }
         break;
     case 'FEINT':
         var trick = prompt("Are you good at tricking people? (YES or NO)").toUpperCase();
         var smart = prompt("Are you smart? (YES or NO)").toUpperCase();
         if(trick === 'YES' && smart === 'YES') {
-            console.log("You trick the guard by throwing a cookie on the ground! The guard drools as he walks to pick it up... You flee from the place, and watch the guard eat the venomous cookie you threw on the ground... I guess he won't be feeling well soon.");
+            confirm("You trick the guard by throwing a cookie on the ground! The guard drools as he walks to pick it up... You flee from the place, and watch the guard eat the venomous cookie you threw on the ground... I guess he won't be feeling well soon.");
         }
         else {
-        console.log("The guard stares at you like you were a stupid donkey, he takes his handcuffs and cuffs you! Next time don't get mindfucked.");
+        confirm("The guard stares at you like you were a stupid donkey, he takes his handcuffs and cuffs you! Next time don't get mindfucked.");
         }
         break;
     default:
-        console.log("I didn't understand your choice. Hit Run and try again, this time picking RUN, FIGHT, or FEINT!");
+        confirm("I didn't understand your choice. Hit Run and try again, this time picking RUN, FIGHT, or FEINT!");
 }
